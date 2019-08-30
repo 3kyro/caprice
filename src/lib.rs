d92cc64a2528c303d37cf7bb89cf00b5dc55cc0e
3kyro <k.papachrisanthou@gmail.com>
2019-08-30 22:45:15 +0200

    autocomplete tests added

--- src/lib.rs
+++ src/lib.rs
@@ -219,8 +219,8 @@ impl Drop for Flags {
     }
 }
 
-// returns the common str slice of a collection od strs
-// returns None if no commin slice can be found
+// returns the common str slice of a collection of str slices
+// returns None if no common slice can be found
 fn return_common_str_from_sorted_collection(collection: Vec<&str>) -> Option<&str> {
     // take the first element of the sorted list and check if the rest of the elements start with
     // if not remove last character and repeat
@@ -253,8 +253,61 @@ fn autocomplete<'a>(word: &str, keywords: &'a Vec<&str>) -> (Vec<&'a str>, Optio
 
     let similar: Vec<&str>;
 
+    // do not return anything until word is atleast one char long
+    if word.is_empty() {
+        return (Vec::with_capacity(0), None);
+    }
+
     similar = keywords.iter().filter(|&x| x.starts_with(word)).map(|x| *x).collect();
 
     (similar.clone(), return_common_str_from_sorted_collection(similar.clone()))
 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn autocomplete_empty_input() {
+        let word = "";
+        let keywords = vec!["non", "important", "for", "this", "test"];
+        assert_eq!(autocomplete(word, &keywords), (Vec::new(), None));
+
+        let word = "random_word";
+        let keywords: Vec<&str> = Vec::new();
+        assert_eq!(autocomplete(word, &keywords),((Vec::new(), None)));
+
+        let word = "";
+        let keywords: Vec<&str> = Vec::new();
+        assert_eq!(autocomplete(word, &keywords),((Vec::new(), None)));
+    }
+
+    #[test]
+    fn autocomplete_returns_as_expected() {
+        // returns correclty empty sets
+        let word = "some_word";
+        let keywords = vec!["non", "important", "for", "this", "test"];
+        assert_eq!(autocomplete(word, &keywords), (Vec::new(), None));
+
+        // returns correclty full sets with full word
+        let word = "some_word";
+        let keywords = vec!["some_word", "some_word", "some_word", "some_word", "some_word"];
+        assert_eq!(autocomplete(word, &keywords), (vec!["some_word", "some_word", "some_word", "some_word", "some_word"], Some("some_word")));
+
+        // returns correclty full sets with one or more char
+        let word = "s";
+        let keywords = vec!["some_word", "some_word", "some_word", "some_word", "some_word"];
+        assert_eq!(autocomplete(word, &keywords), (vec!["some_word", "some_word", "some_word", "some_word", "some_word"], Some("some_word")));
+
+        // returns correclty sets
+        let word = "s";
+        let keywords = vec!["some_word", "some_other_word", "none"];
+        assert_eq!(autocomplete(word, &keywords), (vec!["some_word", "some_other_word"], Some("some_")));
+
+        // returns correclty sets
+        let word = "some_w";
+        let keywords = vec!["some_word", "some_other_word", "none"];
+        assert_eq!(autocomplete(word, &keywords), (vec!["some_word"], Some("some_word")));
+    }
 }
